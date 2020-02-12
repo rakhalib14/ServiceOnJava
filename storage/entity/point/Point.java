9d919415b0e2819a3e49328ff14ad3ff021f4cb4
Alib Rakh <rakhalib14@gmail.com>
2020-02-12 19:57:53 +0600

    creating getters and setters

--- storage/entity/point/Point.java
+++ storage/entity/point/Point.java
@@ -12,5 +12,37 @@ public class Point extends BaseEntity {
     private String content;
     private Boolean active;
     private Date createDate;
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(length = 5000)
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) { this.active = active; }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
 
 }
