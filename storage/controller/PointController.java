ca538bae9b5cfd94fdc202209bd6bc5bf235eb44
Alib Rakh <rakhalib14@gmail.com>
2020-02-13 17:10:31 +0600

    Created PointController

---
+++ storage/controller/PointController.java
@@ -0,0 +1,8 @@
+package storage.controller;
+
+import storage.entity.point.Point;
+import storage.service.point.PointService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
