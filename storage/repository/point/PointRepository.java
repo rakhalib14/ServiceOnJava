9649ca84ead5b3c1e7c895c6f9087f1654366910
Alib Rakh <rakhalib14@gmail.com>
2020-02-13 17:06:27 +0600

    Created PointRepository

---
+++ storage/repository/point/PointRepository.java
@@ -0,0 +1,5 @@
+package storage.repository.point;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import storage.entity.point.Point;
