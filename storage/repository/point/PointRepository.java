dfe2aaa583616c174cd1caddf6261587a515aba9
Alib Rakh <rakhalib14@gmail.com>
2020-02-14 19:52:40 +0600

    Query for PointRepository

--- storage/repository/point/PointRepository.java
+++ storage/repository/point/PointRepository.java
@@ -3,3 +3,15 @@ package storage.repository.point;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import storage.entity.point.Point;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+
+
+@Repository
+public interface PointRepository extends JpaRepository<Announcement, Long> {
+    @Query(value = "select * from point where active=true limit 1", nativeQuery = true)
+    Announcement getLast();
+    List<Announcement> findByActive(Boolean active);
+}
+
