53fe1fb7dfdb245ccca266e7b855539c4b0ab161
Alib Rakh <rakhalib14@gmail.com>
2020-02-14 19:59:12 +0600

    created method findById

--- storage/service/point/pointService.java
+++ storage/service/point/pointService.java
@@ -2,3 +2,23 @@ package storage.service.point;
 
 import storage.entity.point.Point;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import storage.repository.point.PointRepository;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PointService {
+    @Autowired
+    PointRepository pointRepository;
+
+    public Point findById(Long id) {
+        Optional<Point> optionalPoint = pointRepository.findById(id);
+        if(optionalPoint.isPresent()) {
+            return optionalPoint.get();
+        }
+        return null;
+    }
+}
+
