9b95fefb9d7aa0f55da52963e16756a58f47a5e2
Alib Rakh <rakhalib14@gmail.com>
2020-02-18 15:30:13 +0600

    Updated PointController

--- storage/controller/PointController.java
+++ storage/controller/PointController.java
@@ -6,3 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
+@Controller
+@RequestMapping("/points")
+public class PointController {
+    
+  @Autowired
+    PointService pointService;
+
+}
