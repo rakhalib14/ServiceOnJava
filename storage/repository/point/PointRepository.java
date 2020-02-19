2c29f12ddeeafaf47e06a131245534cee0e21f87
Alib Rakh <rakhalib14@gmail.com>
2020-02-19 18:20:41 +0600

    Changed Repository from JPA to PagingAndSorting

--- storage/repository/point/PointRepository.java
+++ storage/repository/point/PointRepository.java
@@ -1,17 +1,21 @@
 package storage.repository.point;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import storage.entity.point.Point;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 
 
 @Repository
-public interface PointRepository extends JpaRepository<Announcement, Long> {
+public interface PointRepository extends PagingAndSortingRepository<Point, Long> {
+
+    Page<Point> findAllByOrderByCreateDateDesc(Pageable pageable);
     @Query(value = "select * from point where active=true limit 1", nativeQuery = true)
-    Announcement getLast();
-    List<Announcement> findByActive(Boolean active);
+    Point getLast();
+    List<Point> findByActive(Boolean active);
 }
 
