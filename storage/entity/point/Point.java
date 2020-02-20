62f03370eef07ef0f766e9bd6b1234039c2369db
Alib Rakh <rakhalib14@gmail.com>
2020-02-20 19:39:03 +0600

    Added Date fields

--- storage/entity/point/Point.java
+++ storage/entity/point/Point.java
@@ -12,6 +12,17 @@ public class Point extends BaseEntity {
     private String content;
     private Boolean active;
     private Date createDate;
+    private Date startDate;
+    private Date endDate;
+
+    public Date getEndDate() { return endDate; }
+
+    public void setEndDate(Date endDate) { this.endDate = endDate; }
+
+    public Date getStartDate() { return startDate; }
+
+    public void setStartDate(Date startDate) { this.startDate = startDate; }
+
     
     public String getTitle() {
         return title;
