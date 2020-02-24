4e79a0ee784f54b7538591d0aaf17ddf00925271
Alib Rakh <rakhalib14@gmail.com>
2020-02-24 18:03:53 +0600

    Changed extended Point Repository

--- storage/repository/point/PointRepository.java
+++ storage/repository/point/PointRepository.java
@@ -4,14 +4,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import storage.entity.point.Point;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 
 
 @Repository
-public interface PointRepository extends PagingAndSortingRepository<Point, Long> {
+public interface PointRepository extends JpaRepository<Point, Long> {
 
     Page<Point> findAllByOrderByCreateDateDesc(Pageable pageable);
     @Query(value = "select * from point where active=true limit 1", nativeQuery = true)
