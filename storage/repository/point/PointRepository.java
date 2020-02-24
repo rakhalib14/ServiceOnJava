11e7c864182a514ae32671f71b29d5855d4e32f0
Alib Rakh <rakhalib14@gmail.com>
2020-02-24 18:09:13 +0600

    Changed conditions and params of query

--- storage/repository/point/PointRepository.java
+++ storage/repository/point/PointRepository.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Repository;
 import storage.entity.point.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import java.util.List;
+import org.springframework.data.repository.query.Param;
+import java.util.Date;
 
 
 
@@ -14,8 +15,8 @@ import java.util.List;
 public interface PointRepository extends JpaRepository<Point, Long> {
 
     Page<Point> findAllByOrderByCreateDateDesc(Pageable pageable);
-    @Query(value = "select * from point where active=true limit 1", nativeQuery = true)
-    Point getLast();
-    List<Point> findByActive(Boolean active);
+    @Query(value = "select * from point where (start_date < :currentDate and end_date > :currentDate) and active=true order by create_date desc limit 1", nativeQuery = true)
+    Point getCurrent(@Param("currentDate") Date currentDate);
+
 }
 
